db59eaa2a824d4a6c7b6a72579efd02a0bc7b87f
Erik Tomlinson <eptomlinson@gmail.com>
2020-09-30 16:06:48 -0500

    added psuedo code and some variables

---
+++ script.js
@@ -0,0 +1,23 @@
+// var quiz questions
+
+// var question1 = prompt("What is black and white and read all over?")
+// should i use an index of questions and answers to make code more dry? if so how? 
+
+// ANUSTART 
+
+// get questions to work first - container divs, LookMaNoHTML
+// get the scores and other vars to work in suite 
+// get the time to work 
+
+// When the page loads, 
+// declare function .timeEl - call Jquary 
+// declare function global variables - userscore, container, leaderboard
+// button with toggle(logic) to start timer/quiz 
+// indentify var(x) 
+// quiz timer starts - setInterval(someFunction, seconds*1000)
+// declare function [array of object(s)] - questions, options, and/or [booleans]: Each question is an object, set of ()
+// array starts - identify for loop  identify the var, i>x, x++. (doesn't have to be < symbol) use === to have a concrete True answer
+var score = 0;
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('submit');
+// var questions = 
